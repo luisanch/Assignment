8c2cadc3933dfda89f2c820277236c9a0b30bcd9
Eduardo-230 <96792014+Eduardo-230@users.noreply.github.com>
2022-01-03 21:05:45 +0100

    Change rotations of  from X to Y
    First rotation around y will take a pos angle and move the mass counterclock wise.
    
    Second rotation around y will take a pos angle and move the mass clockwise.

--- assign.m
+++ assign.m
@@ -76,9 +76,9 @@ c_theta_2 = cosd(theta_2);
 s_theta_2 = sind(theta_2);
 
 A = [c_phi -s_phi 0 0; s_phi c_phi 0 0 ; 0 0 1 0; 0 0 0 1];
-B = [1 0 0 0; 0 c_theta_1 s_theta_1 0 ; 0 -s_theta_1 c_theta_1 0 ;0 0 0 1];
+B = [c_theta_1 0 -s_theta_1 0; 0 1 0 0 ; s_theta_1 0 c_theta_1 0 ;0 0 0 1];
 C = [1 0 0 0; 0 1 0 0 ; 0 0 1 -r; 0 0 0 1];
-D = [1 0 0 0; 0 c_theta_2 -s_theta_2 0; 0 s_theta_2 c_theta_2 0; 0 0 0 1];
+D = [c_theta_2 0 s_theta_2 0; 0 1 0 0 ; -s_theta_2 0 c_theta_2 0 ;0 0 0 1];
 E = [1 0 0 0; 0 1 0 0 ; 0 0 1 -l; 0 0 0 1];  
 T = A*B*C*D*E;
 xyz = T(1:3,4)';
