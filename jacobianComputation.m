802aad689c2ab790239c2d3a399b08032d958c40
luisanch <luissanchezmtn@gmail.com>
2022-01-03 21:46:48 +0100

    Updated Jacobian Computation
    Updated Jacobian Computation

--- jacobianComputation.m
+++ jacobianComputation.m
@@ -3,12 +3,12 @@ clc
 
 syms phi theta_1 theta_2 r l
 
-c_phi = cosd(phi);
-s_phi = sind(phi);
-c_theta_1 = cosd(theta_1);
-s_theta_1 = sind(theta_1);
-c_theta_2 = cosd(theta_2);
-s_theta_2 = sind(theta_2);
+c_phi = cos(phi);
+s_phi = sin(phi);
+c_theta_1 = cos(theta_1);
+s_theta_1 = sin(theta_1);
+c_theta_2 = cos(theta_2);
+s_theta_2 = sin(theta_2);
 
 A = [c_phi -s_phi 0 0; s_phi c_phi 0 0 ; 0 0 1 0; 0 0 0 1];
 B = [c_theta_1 0 -s_theta_1 0; 0 1 0 0 ; s_theta_1 0 c_theta_1 0 ;0 0 0 1];
@@ -16,4 +16,5 @@ C = [1 0 0 0; 0 1 0 0 ; 0 0 1 -r; 0 0 0 1];
 D = [c_theta_2 0 s_theta_2 0; 0 1 0 0 ; -s_theta_2 0 c_theta_2 0 ;0 0 0 1];
 E = [1 0 0 0; 0 1 0 0 ; 0 0 1 -l; 0 0 0 1];
 T = A*B*C*D*E;
-xyz = T(1:3,4); 
+xyz = simplify(T(1:3,4), 'Steps', 10);
+J = simplify(jacobian(xyz, [phi r theta_1 theta_2]), 'Steps', 10);
