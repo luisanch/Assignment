fdc13fc38041ca90691a25c3e9981b75ac3dcdb6
luisanch <luissanchezmtn@gmail.com>
2022-01-04 17:37:29 +0100

    Update jacobianComputation.m

--- jacobianComputation.m
+++ jacobianComputation.m
@@ -1,7 +1,8 @@
 clear variables
+close all
 clc
 
-syms phi theta_1 theta_2 r l
+syms phi theta_1 theta_2 r l m g k r_0
 
 c_phi = cos(phi);
 s_phi = sin(phi);
@@ -20,3 +21,23 @@ E = [1 0 0 0; 0 1 0 0 ; 0 0 1 -l; 0 0 0 1];
 T = A*B*C*D*E;
 xyz = simplify(T(1:3,4), 'Steps', 10);
 J = simplify(jacobian(xyz, [phi r theta_1 theta_2]), 'Steps', 10);
+
+% conservative energies  
+T_OP = A*B*C;
+xyz_OP = simplify(T_OP(1:3,4), 'Steps', 10); 
+u_OP = xyz_OP./(xyz_OP(1)^2+xyz_OP(2)^2+xyz_OP(3)^2)^(1/2);
+u_OP = simplify(u_OP, 'Steps', 10); 
+
+G_f = m*g*xyz(3);
+K_f = 0.5*k*(r-r_0)^2;
+
+U = G_f + K_f;
+U_F_phi = diff(U,phi);
+U_F_theta_1 = diff(U,theta_1);
+U_F_theta_2 = diff(U,theta_2);
+U_F_r = diff(U,r); 
+
+% kinetic energy
+xyz_dot = simplify(J*[phi;theta_1;theta_2;r], 'Steps',10);
+T = simplify(0.5*m*sum(xyz_dot.^2), 'Steps',50);
+
